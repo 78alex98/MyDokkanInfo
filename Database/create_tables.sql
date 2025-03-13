43229f3c582979151b894d35b31f585dcfb957b3
78alex98 <valdesalejandro2004@gmail.com>
2025-03-13 11:39:00 -0400

    Added Categories Table and Related Info Closes #19

--- Database/create_tables.sql
+++ Database/create_tables.sql
@@ -2,6 +2,8 @@
 DROP TABLE IF EXISTS characters;
 DROP TABLE IF EXISTS links_list;
 DROP TABLE IF EXISTS links_character;
+DROP TABLE IF EXISTS categories_list;
+DROP TABLE IF EXISTS categories_character;
 
 -- Create characters table
 CREATE TABLE IF NOT EXISTS characters (
@@ -19,10 +21,26 @@ CREATE TABLE IF NOT EXISTS links_list (
 
 -- Create links_character table
 -- Junction table between links_list and characters
-CREATE TABLE IF NOT EXISTS links_list (
+CREATE TABLE IF NOT EXISTS links_character (
     link_id INTEGER,
-    char id INTEGER,
+    char_id INTEGER,
     FOREIGN KEY (char_id) REFERENCES characters(char_id) ON DELETE CASCADE,
     FOREIGN KEY (link_id) REFERENCES links_list(link_id) ON DELETE CASCADE,
     PRIMARY KEY (char_id, link_id)
+);
+
+-- Stores all categories in game
+CREATE TABLE IF NOT EXISTS categories_list (
+    category_id INTEGER PRIMARY KEY,
+    category_name
+);
+
+-- Create categories_character table
+-- Junction table between categories_list and characters
+CREATE TABLE IF NOT EXISTS categories_character (
+    category_id INTEGER,
+    char_id INTEGER,
+    FOREIGN KEY (char_id) REFERENCES characters(char_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories_list(category_id) ON DELETE CASCADE,
+    PRIMARY KEY (char_id, category_id)
 );
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from Functions.database import *                        # Contains All Functions Related to Database
+from Functions.database import *                                # Contains All Functions Related to Database
 
-db_file = 'Database/dokkan_battle.db'                   # SQLite DataBase File Path
-create_file = 'Database/create_tables.sql'              # SQL File Path for Creating Database Tables
-populate_file = 'Database/populate_tables.sql'          # SQL File Path for Populating Database
-linkslist_file = 'Database/populate_linkslist.sql'      # SQL File Path for Populating Links List Table
+db_file = 'Database/dokkan_battle.db'                           # SQLite DataBase File Path
+create_file = 'Database/create_tables.sql'                      # SQL File Path for Creating Database Tables
+populate_file = 'Database/populate_tables.sql'                  # SQL File Path for Populating Database
+linkslist_file = 'Database/populate_linkslist.sql'              # SQL File Path for Populating Links List Table
+categorieslist_file = 'Database/populate_categorieslist.sql'    # SQL File Path for Populating Categories List Table
 
 # Main Code
 print("Welcome to MyDokkanInfo")
 InitializeDatabase(db_file, create_file)
 PopulateDatabase(db_file, populate_file)
 PopulateDatabase(db_file, linkslist_file)
-print()
+PopulateDatabase(db_file, categorieslist_file)
 PrintCharacters(db_file)
-print()
-PrintLinks(db_file)
