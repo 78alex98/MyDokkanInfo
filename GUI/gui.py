f0bb94d57644d08a8022fd3170a49d9b1bc89b91
78alex98 <valdesalejandro2004@gmail.com>
2025-03-16 17:40:06 -0400

    Added Footer Sections Closes #36 Closes #35 Closes #34 Closes #33

--- GUI/gui.py
+++ GUI/gui.py
@@ -29,8 +29,9 @@ def CreateMainMenuPage(master):
     main_frame = ctk.CTkFrame(master=master)
     main_frame.columnconfigure(0, weight=1)
     main_frame.rowconfigure(0, weight=0)
-    main_frame.rowconfigure((1,3), weight=10)
+    main_frame.rowconfigure(1, weight=10)
     main_frame.rowconfigure(2, weight=1)
+    main_frame.rowconfigure(3, weight=0)
     main_frame.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
     main_frame.grid_propagate(False)
 
@@ -52,10 +53,7 @@ def CreateMainMenuPage(master):
     recent_frame.grid(row=2, column=0, padx=10, pady=10, sticky='nsew')
     recent_frame.grid_propagate(False)
 
-    # Footer Frame
-    footer_frame = ctk.CTkFrame(master=main_frame)
-    footer_frame.grid(row=3, column=0, padx=10, pady=10, sticky='nsew')
-    footer_frame.grid_propagate(False)
+    CreateFooterBar(main_frame, 3, 0, 10, 10)
 
     # Return Whole Page
     return main_frame
@@ -95,6 +93,29 @@ def CreateMenuBar(master, mb_row, mb_column, pad_x, pad_y, window):
     cat_menu.grid(row=0, column=3, padx=menubar_inner_pad, pady=menubar_inner_pad, sticky='ew')
     links_menu = ctk.CTkButton(master=menu_frame, text='Links', fg_color=menubar_innercolor)
     links_menu.grid(row=0, column=4, padx=menubar_inner_pad, pady=menubar_inner_pad, sticky='ew')
+
+def CreateFooterBar(master, mb_row, mb_column, pad_x, pad_y):
+    footerbar_color = 'transparent'
+    footerbar_innercolor = 'transparent'
+    footerbar_inner_padx = 30
+    footerbar_inner_pady = 0
+    footerbar_height = 40
+
+    # Footer Frame
+    footer_frame = ctk.CTkFrame(master=master, height=footerbar_height,fg_color=footerbar_color)
+    footer_frame.grid(row=mb_row, column=mb_column, padx=pad_x, pady=pad_y, sticky='nsew')
+    footer_frame.rowconfigure(0, weight=1)
+    footer_frame.columnconfigure((0,1,2,3), weight=1)
+    footer_frame.grid_propagate(False)
+
+    admin_menu = ctk.CTkButton(master=footer_frame, text='Admin', fg_color=footerbar_innercolor)
+    admin_menu.grid(row=0, column=0, padx=footerbar_inner_padx, pady=footerbar_inner_pady, sticky='sew')
+    admin_menu = ctk.CTkButton(master=footer_frame, text='Updates/News', fg_color=footerbar_innercolor)
+    admin_menu.grid(row=0, column=1, padx=footerbar_inner_padx, pady=footerbar_inner_pady, sticky='sew')
+    admin_menu = ctk.CTkButton(master=footer_frame, text='Settings', fg_color=footerbar_innercolor)
+    admin_menu.grid(row=0, column=2, padx=footerbar_inner_padx, pady=footerbar_inner_pady, sticky='sew')
+    admin_menu = ctk.CTkButton(master=footer_frame, text='Credits', fg_color=footerbar_innercolor)
+    admin_menu.grid(row=0, column=3, padx=footerbar_inner_padx, pady=footerbar_inner_pady, sticky='sew')
 
 def SwitchToHome(window, cur_page):
     cur_page.destroy()
