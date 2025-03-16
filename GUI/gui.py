2160d97151a63f5d57f07be03ca24d05d8f53d44
78alex98 <valdesalejandro2004@gmail.com>
2025-03-16 17:17:39 -0400

    Created Card List Page and Fixed Menu Bar Height Bug Closes #32 Closes #45

--- GUI/gui.py
+++ GUI/gui.py
@@ -18,17 +18,24 @@ def CreateApp():
     window.columnconfigure(0, weight=1)
     window.resizable(True, True)
 
+    # By Default, Launch With Main Menu Page
+    current_page = CreateMainMenuPage(window)
+
+    # Run GUI
+    window.mainloop()
+
+def CreateMainMenuPage(master):
     # Main Menu Frame
-    main_frame = ctk.CTkFrame(master=window)
+    main_frame = ctk.CTkFrame(master=master)
     main_frame.columnconfigure(0, weight=1)
-    main_frame.rowconfigure(0, weight=25)
+    main_frame.rowconfigure(0, weight=0)
     main_frame.rowconfigure((1,3), weight=10)
     main_frame.rowconfigure(2, weight=1)
     main_frame.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
     main_frame.grid_propagate(False)
 
     # Menu Bar Frame
-    CreateMenuBar(main_frame, 0, 0, 10, 10)
+    CreateMenuBar(main_frame, 0, 0, 10, 10, master)
 
     # Title Frame
     title_frame = ctk.CTkFrame(master=main_frame)
@@ -50,23 +57,37 @@ def CreateApp():
     footer_frame.grid(row=3, column=0, padx=10, pady=10, sticky='nsew')
     footer_frame.grid_propagate(False)
 
-    # Run GUI
-    window.mainloop()
+    # Return Whole Page
+    return main_frame
 
-def CreateMenuBar(master, mb_row, mb_column, pad_x, pad_y):
+def CreateCardListPage(master):
+    # Main Menu Frame
+    main_frame = ctk.CTkFrame(master=master)
+    main_frame.columnconfigure(0, weight=1)
+    main_frame.rowconfigure(0, weight=0)
+    main_frame.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
+    main_frame.grid_propagate(False)
+
+    # Menu Bar Frame
+    CreateMenuBar(main_frame, 0, 0, 10, 10, master)
+
+    return main_frame
+
+def CreateMenuBar(master, mb_row, mb_column, pad_x, pad_y, window):
     menubar_color = 'gray'
     menubar_innercolor = 'gray'
     menubar_inner_pad = 5
+    menubar_height = 50
 
-    menu_frame = ctk.CTkFrame(master=master, fg_color=menubar_color)
+    menu_frame = ctk.CTkFrame(master=master, height=menubar_height, fg_color=menubar_color)
     menu_frame.grid(row=mb_row, column=mb_column, padx=pad_x, pady=pad_y, sticky='nsew')
     menu_frame.rowconfigure(0, weight=1)
     menu_frame.columnconfigure((0,1,2,3,4), weight=1)
     menu_frame.grid_propagate(False)
 
-    home_menu = ctk.CTkButton(master=menu_frame, text='Home', fg_color=menubar_innercolor)
+    home_menu = ctk.CTkButton(master=menu_frame, text='Home', fg_color=menubar_innercolor, command = lambda : SwitchToHome(window, master))
     home_menu.grid(row=0, column=0, padx=menubar_inner_pad, pady=menubar_inner_pad, sticky='ew')
-    card_menu = ctk.CTkButton(master=menu_frame, text='Cards', fg_color=menubar_innercolor)
+    card_menu = ctk.CTkButton(master=menu_frame, text='Cards', fg_color=menubar_innercolor, command = lambda : SwitchToCardList(window, master))
     card_menu.grid(row=0, column=1, padx=menubar_inner_pad, pady=menubar_inner_pad, sticky='ew')
     team_menu = ctk.CTkButton(master=menu_frame, text='Teams', fg_color=menubar_innercolor)
     team_menu.grid(row=0, column=2, padx=menubar_inner_pad, pady=menubar_inner_pad, sticky='ew')
@@ -74,3 +95,13 @@ def CreateMenuBar(master, mb_row, mb_column, pad_x, pad_y):
     cat_menu.grid(row=0, column=3, padx=menubar_inner_pad, pady=menubar_inner_pad, sticky='ew')
     links_menu = ctk.CTkButton(master=menu_frame, text='Links', fg_color=menubar_innercolor)
     links_menu.grid(row=0, column=4, padx=menubar_inner_pad, pady=menubar_inner_pad, sticky='ew')
+
+def SwitchToHome(window, cur_page):
+    cur_page.destroy()
+    cur_page = CreateMainMenuPage(window)
+    print('Switched to Home')
+
+def SwitchToCardList(window, cur_page):
+    cur_page.destroy()
+    cur_page = CreateCardListPage(window)
+    print('Switched to Card List')
